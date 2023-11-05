febdadd8f1f6a9f2b69d77c2a823b88d97a662b4
ysjeung <yunsukjeung@snu.ac.kr>
2023-11-05 18:47:41 +0900

    .

--- install_libs_windows_vs17_release.bat
+++ install_libs_windows_vs17_release.bat
@@ -3,7 +3,7 @@ REM Set the paths
 set CMAKE_PATH="C:\Program Files\CMake\bin\cmake.exe"
 set SCRIPT_DIR=%~dp0
 set PROJECT_DIR=%SCRIPT_DIR%
-set INSTALL_DIR=%PROJECT_DIR%\lib
+set INSTALL_DIR=%PROJECT_DIR%\libs
 
 REM Navigate to the project directory
 cd %PROJECT_DIR%
@@ -13,7 +13,7 @@ if not exist build mkdir build
 cd build
 
 REM Run CMake for the project with a custom install prefix
-%CMAKE_PATH% -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..
+%CMAKE_PATH% -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ../install
 
 REM Optionally, build the project using CMake
 %CMAKE_PATH% --build . --config Release
