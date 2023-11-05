1004fb991fb27d053f4c7abf3a06b9dbde8639dd
ysjeung <yunsukjeung@snu.ac.kr>
2023-11-05 18:38:42 +0900

    tbb build script

---
+++ install_libs_windows_vs17.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Set the paths
+set CMAKE_PATH="C:\Program Files\CMake\bin\cmake.exe"
+set SCRIPT_DIR=%~dp0
+set PROJECT_DIR=%SCRIPT_DIR%
+set INSTALL_DIR=%PROJECT_DIR%\lib
+
+REM Navigate to the project directory
+cd %PROJECT_DIR%
+
+REM Create a build directory if it doesn't exist
+if not exist build mkdir build
+cd build
+
+REM Run CMake for the project with a custom install prefix
+%CMAKE_PATH% -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..
+
+REM Optionally, build the project using CMake
+%CMAKE_PATH% --build . --config Release
+
+REM Optionally, install the project to the specified install directory
+%CMAKE_PATH% --install . --config Release
+
+echo Done!
+pause
