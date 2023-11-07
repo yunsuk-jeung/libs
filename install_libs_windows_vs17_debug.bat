025c08973786b6e485f3a463ce234fa3add1682b
dan <ysjueng@maxst.com>
2023-11-07 12:21:02 +0900

    modified cmakelist and bat

--- install_libs_windows_vs17_debug.bat
+++ install_libs_windows_vs17_debug.bat
@@ -1,28 +1,22 @@
 @echo off
-REM Set the paths
-set CMAKE_PATH="C:\Program Files\CMake\bin\cmake.exe"
-set SCRIPT_DIR=%~dp0
-set PROJECT_DIR=%SCRIPT_DIR%
-set INSTALL_DIR=%PROJECT_DIR%\libs_win
+
+set CURR_DIR=%~dp0
 set BUILD_TYPE=Debug
 
-REM Navigate to the project directory
-cd %PROJECT_DIR%
+cd %CURR_DIR%
+call win_scripts\install_eigen3_3_7.bat %BUILD_TYPE%
+echo Eigen Done!
 
-REM Create a build directory if it doesn't exist
-if not exist build mkdir build
-cd build
+cd %CURR_DIR%
+call win_scripts\intsall_tbb_2021_11_0_rc1.bat %BUILD_TYPE%
+echo spdlog Done!
 
-IF EXIST CMakeCache.txt DEL /F CMakeCache.txt
+cd %CURR_DIR%
+call win_scripts\intsall_spdlog1_12_0.bat %BUILD_TYPE%
+echo tbb Done!
 
-REM Run CMake for the project with a custom install prefix
-%CMAKE_PATH% -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ../install_cmake
+cd %CURR_DIR%
+call win_scripts\intsall_json3_11_2.bat %BUILD_TYPE%
+echo tbb Done!
 
-REM Optionally, build the project using CMake
-%CMAKE_PATH% --build . --config %BUILD_TYPE%
-
-REM Optionally, install the project to the specified install directory
-%CMAKE_PATH% --install . --config %BUILD_TYPE%
-
-echo Done!
 pause
